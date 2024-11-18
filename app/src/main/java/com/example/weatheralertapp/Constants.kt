27899bc853fc05d0ac0a438c065f6034e84485ca
“Segun <Oluwatayoo08@gmail.com>
2024-11-18 13:26:49 +0000

    Added a constants file for saving of the base url and pi key for the weather and alerts

---
+++ app/src/main/java/com/example/weatheralertapp/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.weatheralertapp
+
+object Constants {
+    const val TOMORROW_API_KEY = "AFlakHN7Q8cwTJNX7wqFQbMDIdZEiOH9"
+    const val TOMORROW_BASE_URL = "https://api.tomorrow.io/v4/"
+}
