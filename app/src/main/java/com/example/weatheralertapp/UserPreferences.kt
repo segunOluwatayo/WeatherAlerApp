9e5da45255c667285242a6ec9b977d94001b2e2b
“Segun <Oluwatayoo08@gmail.com>
2024-11-27 17:35:29 +0000

    Added a theme picker to select light or dark mode preference

--- app/src/main/java/com/example/weatheralertapp/UserPreferences.kt
+++ app/src/main/java/com/example/weatheralertapp/UserPreferences.kt
@@ -1,4 +1,4 @@
-
+// File: UserPreferences.kt
 package com.example.weatheralertapp
 
 import androidx.room.Entity
@@ -7,9 +7,16 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "user_preferences")
 data class UserPreferences(
     @PrimaryKey val id: Int = 1, // Singleton pattern
-    val storms: Boolean = true,
-    val heatwaves: Boolean = true,
+    val fires: Boolean = true,
+    val wind: Boolean = true,
+    val winter: Boolean = true,
+    val thunderstorms: Boolean = true,
     val floods: Boolean = true,
-    val coldSnaps: Boolean = true,
-    val notificationsEnabled: Boolean = true
+    val temperature: Boolean = true,
+    val tropical: Boolean = true,
+    val marine: Boolean = true,
+    val fog: Boolean = true,
+    val tornado: Boolean = true,
+    val notificationsEnabled: Boolean = true,
+    val themeMode: ThemeMode = ThemeMode.SYSTEM
 )
