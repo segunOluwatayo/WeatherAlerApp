257691c991581612a117e69d46774cd1f3f8d977
“Segun <Oluwatayoo08@gmail.com>
2024-11-28 08:07:58 +0000

    Modified this to allow setting view model take effect on the whole ui theme preference setting

--- app/src/main/java/com/example/weatheralertapp/NavigationGraph.kt
+++ app/src/main/java/com/example/weatheralertapp/NavigationGraph.kt
@@ -1,12 +1,15 @@
 package com.example.weatheralertapp
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 @Composable
-fun NavigationGraph(navController: NavHostController, modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier) {
+fun NavigationGraph(navController: NavHostController, modifier: Modifier = Modifier) {
+    val settingsViewModel: SettingsViewModel = viewModel()
     NavHost(
         navController = navController,
         startDestination = Screen.Home.route,
@@ -19,7 +22,7 @@ fun NavigationGraph(navController: NavHostController, modifier: androidx.compose
             AlertsScreen()
         }
         composable(Screen.Settings.route) {
-            SettingsScreen()
+            SettingsScreen( viewModel = settingsViewModel)
         }
     }
 }
